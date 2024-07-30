e7f224b38685eb9a9070df8b8da2cdfe649fe937
Prateek Thakur <prateek.thakur6@gmail.com>
2024-07-30 14:57:37 +0530

    Update webPushConfig.js

--- config/webPushConfig.js
+++ config/webPushConfig.js
@@ -1,12 +1,12 @@
 const webPush = require('web-push');
 
 const vapidKeys = {
-  publicKey: 'BNOubIkxQQa1y5fpM2MBJ7FZI69__i113K1sIiXhO2YbM737-ZlXi8KM9fHi3FgJTMDTe_xMd19MlTeyueEa6cU',
-  privateKey: '7NVd7nIO5qXx3KitMRW3Ma9gj7dTTXALxbIhd8aKv-U'
+  publicKey: 'public',
+  privateKey: 'private'
 };
 
 webPush.setVapidDetails(
-  'mailto:prateek.thakur6@gmail.com',
+  'mailto:your-email-id',
   vapidKeys.publicKey,
   vapidKeys.privateKey
 );
