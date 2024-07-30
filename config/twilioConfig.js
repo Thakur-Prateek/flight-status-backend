2820a5f63f523ca62570d1acc5e0f0a4d203b976
Prateek Thakur <prateek.thakur6@gmail.com>
2024-07-30 14:57:01 +0530

    Update twilioConfig.js

--- config/twilioConfig.js
+++ config/twilioConfig.js
@@ -1,5 +1,5 @@
-const accountSid = 'ACf97b5803794dad845e6a8abdf6d7f606';
-const authToken = 'e1824bfe47c5aabf9ccfc59c5e026789';
+const accountSid = 'SID';
+const authToken = 'authToken';
 const client = require('twilio')(accountSid, authToken);
 
 const twilioPhoneNumber = '+19383003403';
